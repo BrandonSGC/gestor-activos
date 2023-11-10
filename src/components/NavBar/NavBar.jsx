593e22df14be73a8fe92f7f57835a0246d8d8d51
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-11-10 16:50:42 -0600

    Agregando navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import './navbar.css'
+
+const links = [
+    {
+        name: 'Login',
+        href: '/login'
+    },
+];
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                {links.map((link, index) => (
+                    <Link key={index} to={link.href} className="navbarItem" > {link.name}</Link>
+                ))}
+            </ul>
+
+        </nav>
+    )
+}
+
+export default NavBar;
