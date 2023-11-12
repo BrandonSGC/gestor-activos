a6302edd4dd641d7e86f3b2a74dd0b19b13cf227
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-11-12 13:58:56 -0600

    Agregando frontend del login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,15 @@
-
+import '../assets/styles/login.css'
 
 const LoginPage = () => {
     return (
-        <>
-            <h1>Login</h1>
-        </>
+        <div className="login-container">
+            <label htmlFor="mail" className='label-login'>Correo</label>
+            <input id="mail" className='input-login' type="text" />
+            <label htmlFor="password" className='label-login'>Contraseña</label>
+            <input id="password" className='input-login' type="text" />
+            <a href="">¿Olvidaste tu contraseña?</a>
+            <button className='btn-login' >Iniciar sesión</button>
+        </div>
     )
 }
 
