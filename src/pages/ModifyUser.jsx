cb74d5e493f0a9bfb3af38a74b683f0314fe2e56
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-11-13 19:39:38 -0600

    Correcion de error de importacion

--- src/pages/ModifyUser.jsx
+++ src/pages/ModifyUser.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../assets/styles/modifyuser.css';
+import '../assets/styles/modifyUser.css';
 
 const ModifyUserPage = () => {
     const [formData, setFormData] = useState({
