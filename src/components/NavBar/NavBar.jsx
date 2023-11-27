37b93aabc356c4b70c47321a7ee0c90faf3ca3c2
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-11-26 23:32:26 -0600

    Navbar con contexto

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,16 @@
+import React, { useContext } from 'react';
 import { Link } from "react-router-dom";
 import './navbar.css'
+import { UserContext } from '../../context/UserContext';
 
-const links = [
+const defaultLinks = [
     {
         name: 'Login',
         href: '/login'
-    },
+    }
+];
+
+const adminLinks = [
     {
         name: 'Registrar Usuarios',
         href: '/register'
@@ -33,20 +38,63 @@ const links = [
     {
         name: 'Insertar Activos',
         href: '/admin/insert-items'
+    },
+    {
+        name: 'Tabla de usuarios',
+        href: '/admin/users'
     }
 ];
 
+const usersLinks = [
+    {
+        name: 'Activos',
+        href: '/assets'
+    },
+    {
+        name: 'Bodega de tránsito',
+        href: '/assets/transit'
+    },
+    {
+        name: 'Bodega de desecho',
+        href: '/assets/waste'
+    },
+    {
+        name: 'Modificar Usuario',
+        href: '/modify-user/0'
+    },
+
+];
 const NavBar = () => {
+    const { user } = useContext(UserContext);
+
+    let linksToShow = defaultLinks;
+
+    if (user) {
+        switch (user.RolID) {
+            case 1:
+            case 2:
+                linksToShow = adminLinks;
+                break;
+            case 3:
+            case 4:
+                linksToShow = usersLinks;
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <nav className="navbar">
             <ul>
-                {links.map((link, index) => (
-                    <Link key={index} to={link.href} className="navbarItem" > {link.name}</Link>
+                {linksToShow.map((link, index) => (
+                    <Link key={index} to={link.href} className="navbarItem">
+                        {link.name}
+                    </Link>
                 ))}
             </ul>
-
         </nav>
-    )
-}
+    );
+};
 
 export default NavBar;
