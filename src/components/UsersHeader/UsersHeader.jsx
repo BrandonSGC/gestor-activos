6082d891b45d2e425069c7ad7dbb7ade2e11dfa8
Kendall Madrigal <kendallmadrigal14@gmail.com>
2023-11-26 21:50:41 -0600

    Users Card

---
+++ src/components/UsersHeader/UsersHeader.jsx
@@ -0,0 +1,13 @@
+
+export const UsersHeader = ({ title }) => {
+    return (
+        <header className="assets__header">
+            <div className="assets__display">
+                <p className="assets__heading">{title}</p>
+                <form className="assets__form">
+
+                </form>
+            </div>
+        </header>
+    );
+};
